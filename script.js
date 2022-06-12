979fd09f3167299a80c2e91e6cdc74d55649d8d8
Andrew Baxter <asbaxter1@gmail.com>
2022-06-12 17:22:10 -0400

    moved some lines of code to make it more readable no changes to actual functionalilty

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function generatePassword(){
 
     var lowercase = confirm("Would you like for your password to contain lowercase letters.?");
     var uppercase = confirm("would you like for your password to contain uppercase letters?");
-    var numerical = confirm("would you like for your password to contain numbers?");
     var special = confirm("would you like for your password to contain special characters ");
+    var numerical = confirm("would you like for your password to contain numbers?");
 
     // -------------------- arrays containing character options -------------------------------------------
-    var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     var lowerCasedCharacters = ['a','b','c','d', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
     var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   
