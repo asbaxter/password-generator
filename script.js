60d19e7b324239fbfa7c1dbb8114f31a0f9de44e
Andrew Baxter <asbaxter1@gmail.com>
2022-06-12 12:40:40 -0400

    added basic password logic, program creates password in console log based off user input

--- script.js
+++ script.js
@@ -5,14 +5,7 @@ function generatePassword(password){
     var lowercase = confirm("Would you like for your password to contain lowercase letters.?");
     var uppercase = confirm("would you like for your password to contain uppercase letters?");
     var numerical = confirm("would you like for your password to contain numbers?");
-    var specail = confirm("would you like for your password to contain specail characters ")
-
-  // console logs for testing purposes  
-    console.log(passwordLength);
-    console.log(lowercase);
-    console.log(uppercase);
-    console.log(numerical);
-    console.log(specail);
+    var special = confirm("would you like for your password to contain specail characters ");
 
     // -------------------- arrays containing character options -------------------------------------------
     var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -20,6 +13,34 @@ function generatePassword(password){
     var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
     var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+
+  // --------------------- loop runing program for length of the password ---------------------
+    for (var i = 0; i < passwordLength; i++){
+
+     var characterType = Math.floor(Math.random() * 4);
+
+     if (characterType === 0 && lowercase === true){
+        var character = Math.floor(Math.random() * 26);
+        console.log(lowerCasedCharacters[character]);
+     }
+     else if (characterType === 1 && uppercase === true){
+      var character = Math.floor(Math.random() * 26);
+        console.log(upperCasedCharacters[character]);
+     }
+     else if (characterType === 2 && special === true){
+      var character = Math.floor(Math.random() * 23);
+        console.log(specialCharacters[character]);
+     }
+     else if (characterType === 3 && numerical === true){
+      var character = Math.floor(Math.random() * 10);
+        console.log(numericCharacters[character]);
+     }
+     else {
+      // ----------- loop continues if random number picks unpickable option based off user choice ---------------------
+      i = i - 1;
+     }
+
+    }
 };
 
 // Get references to the #generate element
